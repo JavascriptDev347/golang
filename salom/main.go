e6669c8a95e79ca52047ed1fb71ac52f408afe33
RustambekJavascript <rustambekeshboriyev@gmail.com>
2025-08-19 18:19:39 +0500

    finish lessons

--- salom/main.go
+++ salom/main.go
@@ -1,8 +1,37 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"time"
+)
 
 // channel => go routinela aloqasi uchun
 func main() {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
 
+	go func() {
+		for {
+			channel1 <- "Tez"
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+
+	go func() {
+		for {
+			channel2 <- "Sekin"
+			time.Sleep(2 * time.Second)
+		}
+	}()
+
+	for {
+		select {
+		case message1 := <-channel1:
+			fmt.Println(message1)
+		case message2 := <-channel2:
+			fmt.Println(message2)
+		default:
+			fmt.Println("Ma'lumot yo'q")
+		}
+	}
 }
